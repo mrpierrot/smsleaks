7699b37d18a15e158ec84eaec1e9bb9449db442e
Pierrot <pierre@casusludi.com>
2015-09-29 23:05:54 +0200

    changement sur le path

--- src/app/components/game/states/init/controller.js
+++ src/app/components/game/states/init/controller.js
@@ -1,11 +1,11 @@
 module.exports = module.exports = function(angular,config){
     return function ($scope,preloader,$state) {
     	preloader.start({
-    		"actors":"/assets/data/actors.json",
-    		"act01":"/assets/data/fr/act01.json",
-    		"act02":"/assets/data/fr/act02.json",
-    		"act03":"/assets/data/fr/act03.json",
-    		"act04":"/assets/data/fr/act04.json"
+    		"actors":"./assets/data/actors.json",
+    		"act01":"./assets/data/fr/act01.json",
+    		"act02":"./assets/data/fr/act02.json",
+    		"act03":"./assets/data/fr/act03.json",
+    		"act04":"./assets/data/fr/act04.json"
 
     	},function(result) {
     		$state.go('game-home');
